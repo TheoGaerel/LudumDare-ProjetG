e607af70f8bd8219c81f93a2c33f8e015bac10d2
TheoGaerel <devangelcorp@gmail.com>
2023-01-08 15:32:14 +0100

    Camera Rework

---
+++ Assets/Scripts/Sc_MainCamera.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Sc_MainCamera : MonoBehaviour
+{
+    public enum CameraDirection
+    {
+        North,
+        East,
+        South,
+        West
+    }
+
+
+    [SerializeField]
+    private Transform trsf_cameraFollow;
+    public float SmoothTime = 0.3f;
+    private Vector3 velocity = Vector3.zero;
+
+    [SerializeField]
+    private Transform trsf_North;
+    [SerializeField]
+    private Transform trsf_East;
+    [SerializeField]
+    private Transform trsf_South;
+    [SerializeField]
+    private Transform trsf_West;
+
+    public CameraDirection currentDirection = CameraDirection.North;
+
+
+    private void Update()
+    {
+        if (Keyboard.current.upArrowKey.wasPressedThisFrame) SetCameraDirection(CameraDirection.North);
+        if (Keyboard.current.leftArrowKey.wasPressedThisFrame) SetCameraDirection(CameraDirection.West);
+        if (Keyboard.current.rightArrowKey.wasPressedThisFrame) SetCameraDirection(CameraDirection.East);
+        if (Keyboard.current.downArrowKey.wasPressedThisFrame) SetCameraDirection(CameraDirection.South);
+    }
+
+
+    void LateUpdate()
+    {
+        Vector3 targetPosition = trsf_cameraFollow.position;
+        this.transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, SmoothTime);
+    }
+
+    public void SetCameraDirection(CameraDirection cameraDirection)
+    {
+        Transform trsfDirection = null;
+
+        switch (cameraDirection)
+        {
+            case CameraDirection.North:
+                trsfDirection = trsf_North;
+                break;
+            case CameraDirection.East:
+                trsfDirection = trsf_East;
+                break;
+            case CameraDirection.South:
+                trsfDirection = trsf_South;
+                break;
+            case CameraDirection.West:
+                trsfDirection = trsf_West;
+                break;
+        }
+
+        StartCoroutine(RoutineChangeCameraDirection(trsfDirection));
+    }
+
+    private IEnumerator RoutineChangeCameraDirection(Transform trsf)
+    {
+        float t = 0;
+        while (t < 1)
+        {
+            t += Time.deltaTime;
+            Camera.main.transform.position = Vector3.Lerp(Camera.main.transform.position, trsf.position, t);
+            Camera.main.transform.rotation = Quaternion.Lerp(Camera.main.transform.rotation, trsf.rotation, t);
+            yield return null;
+        }
+    }
+}
